50996926432b8fd4d5712bb1212f5666780f29df
Pattaradanai888 <pattaradanai_t@cmu.ac.th>
2025-03-23 20:17:33 +0700

    update service __init__

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,8 +1,6 @@
 from .auth_service import AuthService
 from .search_service import SearchService
 from .recommend_service import RecommendService
-from .recommend_service_option1 import RecommendServiceOption1
 from .recommend_service_option2 import RecommendServiceOption2
-from .recommend_service_new import RecommendServiceNew
 
-__all__ = ['AuthService' , 'SearchService' , 'RecommendService' , 'RecommendServiceOption1' , 'RecommendServiceOption2' , 'RecommendServiceNew']
+__all__ = ['AuthService' , 'SearchService' , 'RecommendService' , 'RecommendServiceOption2']
